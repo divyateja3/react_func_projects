db4c143858b35d380272d7f5c37ff1b2eab54adf
divyateja3 <Divyatej2002@gmail.com>
2023-06-17 02:03:09 +0530

    2setupsDone/modified

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -1,10 +1,58 @@
-import React, { useState } from 'react'
-import SingleColor from './SingleColor'
-
-import Values from 'values.js'
+import React, { useState } from 'react';
+import SingleColor from './SingleColor';
+import Values from 'values.js';
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setColor] = useState('');
+  const [error, setError] = useState(false);
+  const [colorCount, setColorCount] = useState(10); // State for the number of colors
+  const [list, setList] = useState(new Values('#f15025').all(colorCount));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(colorCount); // Use the dynamic colorCount value
+      setList(colors);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <section className='container'>
+        <h3>PalettePro</h3>
+        <form onSubmit={handleSubmit} className='form-container'>
+          <div className='input-container'>
+            <input
+              type='text'
+              value={color}
+              onChange={(e) => setColor(e.target.value)}
+              placeholder='#f15025'
+              className={`${error ? 'error' : null}`}
+              />
+          </div>
+          <div className='input-container'>
+
+            <input
+              type='number'
+              value={colorCount}
+              onChange={(e) => setColorCount(parseInt(e.target.value))}
+              min='1'
+              max='50'
+            />
+          </div>
+          <button className='btn'>Enter</button>
+        </form>
+      </section>
+      <section className='colors'>
+        {list.map((color, index) => (
+          <SingleColor key={index} {...color} index={index} hex={color.hex} />
+        ))}
+      </section>
+    </>
+  );
 }
 
-export default App
+export default App;
--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -1,8 +1,28 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
-const SingleColor = () => {
-  return <h4>single color</h4>
+const SingleColor = ({ rgb, weight, index,hex }) => {
+  const [alert, setAlert] = useState(false)
+  const bcg = rgb.join(',')
+  const hexvalue = `#${hex}`
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false)
+    }, 3000)
+    return () => clearTimeout(timeout) 
+  },[alert])
+  return (
+    <article className={`color ${index > 8 && 'color-light'}`} style={{ backgroundColor: `rgb(${bcg})` }} 
+    >
+      <p className='color-value'>{hexvalue}</p>
+      <p className='percent-value'>{weight}%</p>
+      <button className = 'btncopy' onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hexvalue)
+      }}> copy here </button>
+      {alert && <p className = 'alert'> ready to get pasted </p>}
+    </article>
+  )
 }
 
 export default SingleColor
