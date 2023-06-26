06c5cdf60ab1ec533498edf501df7a142a888541
divyateja3 <Divyatej2002@gmail.com>
2023-06-26 21:29:08 +0530

    12,13,14done

--- 13-stripe-submenus/setup/src/Hero.js
+++ 13-stripe-submenus/setup/src/Hero.js
@@ -1,8 +1,30 @@
 import React from 'react'
 import phoneImg from './images/phone.svg'
+import { useGlobalContext } from './context'
 
 const Hero = () => {
-  return <h2>hero component</h2>
+  const data = useGlobalContext()
+  const {closeSubmenu} = useGlobalContext()
+  return (
+    <section className='hero' onMouseOver={closeSubmenu}>
+      <div className='hero-center'>
+        <article className='hero-info'>
+          <h1>Internet payment infrastructure</h1>
+          <p>
+            Stripe is the go-to payments platform for
+            businesses of all sizes that want to accept
+            payments, send payouts, and manage their businesses online.
+          </p>
+          <button className='btn'>
+            Get Started
+          </button>
+        </article>
+        <article className='hero-images'>
+          <img src = {phoneImg} className='phone-img' alt='phone' />
+        </article>
+      </div>
+    </section>
+  )
 }
 
 export default Hero
